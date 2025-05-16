acb7a874f3f690c79bd99c055ad470ae250b3c86
SergioMadrid522 <sergiofabian.madrid@hotmail.com>
2025-05-15 22:41:33 -0600

    Fix problems with design

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,49 +1,35 @@
 const navbarMenu = [
     {
         imageLogoPath: '',
-        colorIcon: '#000000',
         accountIcon: 'M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z',
         classLogin: "user-info"
+    },
+    {
+        imageLogoPath: '',
+        cartIcon: 'M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z',
+
     }
 ]
-const navbar = document.querySelector('nav');
+const bodyDocument = document.body;
 
 navbarMenu.forEach(nav => {
-    const menu = document.createElement('ul');
-    menu.innerHTML = `
-        <li><a href=""><img src="${menu.imageLogoPath}" alt="R&M Logo" /></a></li>
-        <li>
-            <div id="${nav.classLogin}">
-                <a href="pages/login.html" id="index">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="${menu.colorIcon}" class="bi bi-person" viewBox="0 0 16 16">
-                    <path d="${nav.accountIcon}"/>
-                    </svg>
-                </a>
-            </div>
-        </li>
-    `;
-    navbar.appendChild(menu);
-});
-
-{/* 
-
-<ul>
-    <li><a href=""><img src="" alt="R&M Logo" /></a></li>
-    <li>
+    const navbar = document.createElement('nav');
+    navbar.classList.add('nav');
+    navbar.innerHTML = `
+        <div id="logo"><a href=""><img src="" alt="R&M Logo" /></a></div>
         <div id="user-info">
-            <a href="pages/login.html" id="index">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-                </svg>
-            </a>
+          <a href="pages/login.html" id="user">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+              <path d="${nav.accountIcon}"/>
+            </svg>
+          </a>
+          <a href="" id="user">
+            <svg xmlns="http://www.w3.org/2000/svg"fill="currentColor" class="bi bi-bag-dash" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M5.5 10a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5"/>
+              <path d=""/>
+            </svg>
+          </a>
         </div>
-    </li>
-</ul>
-
-<!-- <li>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-dash" viewBox="0 0 16 16">
-<path fill-rule="evenodd" d="M5.5 10a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5"/>
-<path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-</svg>
-</li> -->
-*/}
+    `;
+    bodyDocument.appendChild(navbar);
+});
